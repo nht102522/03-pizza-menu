d1b6578ef736b4e3bca97cf113bbab235b418a40
Nam Trinh <nht35@drexel.edu>
2025-11-09 23:49:02 -0500

    finish lesson 50 - Extracting JSX into a New Component

--- src/index.js
+++ src/index.js
@@ -117,7 +117,7 @@ function Footer() {
   const hour = new Date().getHours();
   console.log(hour);
   const openHour = 10;
-  const closeHour = 22;
+  const closeHour = 24;
   const isOpen = hour >= openHour && hour <= closeHour;
 
   return (
@@ -125,16 +125,21 @@ function Footer() {
       {/* We're open until {new Date().toLocaleDateString()}. Come visit us or order
       online. */}
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between ⏰{openHour}:00 and {closeHour}:00
         </p>
       )}
     </footer>
+  );
+}
+function Order({ closeHour }) {
+  return (
+    <div className="order">
+      <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
 
